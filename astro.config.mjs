ab3efdb668328d0603ae531204dd40306fdb566b
fots <facundotomatis@gmail.com>
2024-08-24 15:47:15 -0300

    feat(gral): added quantum circuit component, added optional languages content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,44 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+    integrations: [
+        starlight({
+            title: 'Quantum Katas',
+            defaultLocale: 'es',
+            //social: {
+            //  github: 'https://github.com/fotscode',
+            //},
+            locales: {
+                es: {
+                    label: 'Español',
+                    lang: 'es',
+                },
+                en: {
+                    label: 'English',
+                    lang: 'en',
+                },
+            },
+            sidebar: [
+                {
+                    label: 'Algoritmos',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Deutsch-Jozsa', slug: 'algorithms/deutsch-jozsa' },
+                    ],
+                    translations: {
+                        en: 'Algorithms',
+                    },
+                },
+                {
+                    label: 'Referencias',
+                    autogenerate: { directory: 'reference' },
+                    translations: {
+                        en: 'References',
+                    },
+                },
+            ],
+        }),
+    ],
+})
--- src/content/docs/es/reference/example.md
+++ src/content/docs/es/reference/example.md
@@ -1,5 +1,5 @@
 ---
-title: Example Reference
+title: Ejemplo referencias
 description: A reference page in my new Starlight docs site.
 ---
 
