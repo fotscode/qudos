0d965d346be309f5ec3ab3ae4600d8bfd623df49
fots <facundotomatis@gmail.com>
2025-04-18 01:55:52 -0300

    feat(gates): added conditional hadamard info

--- src/code-snippets/hadamard.js
+++ src/code-snippets/hadamard.js
@@ -5,3 +5,4 @@ qc = QuantumCircuit(1)
 qc.h(0) # aplica la puerta Hadamard al qubit 0
 `
 
+export const chadamard_quirk_example=`https://algassert.com/quirk#circuit=%7B%22cols%22%3A%5B%5B%22~3vm4%22%5D%2C%5B%22Amps2%22%2C1%2C%22Amps3%22%5D%2C%5B%5D%2C%5B%22%E2%80%A2%22%2C%22H%22%5D%2C%5B1%2C1%2C%22%E2%97%A6%22%2C%22H%22%2C%22%E2%80%A2%22%5D%2C%5B%22Amps2%22%2C1%2C%22Amps3%22%5D%5D%2C%22gates%22%3A%5B%7B%22id%22%3A%22~3vm4%22%2C%22name%22%3A%22r%5Et%22%2C%22circuit%22%3A%7B%22cols%22%3A%5B%5B%22X%5Et%22%5D%2C%5B%22Y%5Et%22%5D%2C%5B%22Z%5Et%22%5D%5D%7D%7D%5D%2C%22init%22%3A%5B0%2C0%2C0%2C0%2C1%5D%7D`
